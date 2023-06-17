3bdf0b58a86d6d0e3709edb0aabd71214610e702
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2023-06-17 21:22:58 +0600

    feat: added the recognizible patterns content

--- src/components/StevenSignal/RecognizablePatterns/RecognizablePatterns.tsx
+++ src/components/StevenSignal/RecognizablePatterns/RecognizablePatterns.tsx
@@ -1,15 +1,37 @@
-import { Text, Flex, Grid, GridItem } from "@chakra-ui/react";
+import CustomIcon from '@/components/shared/CustomIcon'
+import { Flex, Text } from '@chakra-ui/react'
 
-const RecognizablePatterns = () => {
-  return (
-    <Grid py="25px" px="48px" bg="#3475EF" borderRadius="8px">
-      <GridItem>
-        <Flex gap="55px">
-          <Text>Распознаваемые паттерны</Text>
-        </Flex>
-      </GridItem>
-    </Grid>
-  );
-};
+const RecognizablePatterns = () => (
+  <Flex
+    bg="#3475EF"
+    borderRadius="8px"
+    py={{ base: '32px', md: '25px' }}
+    px={{ base: '20px', md: '48px' }}
+    wrap="wrap"
+    alignItems="flex-start"
+    gap={{ base: '32px', sm: '50px' }}
+  >
+    <Flex
+      gap={{ base: '32px', sm: '50px' }}
+      alignItems="flex-start"
+      wrap="wrap"
+    >
+      <Text
+        textAlign={{ base: 'center', md: 'left' }}
+        color="#fff"
+        fontWeight={700}
+        fontSize={{ base: '24px', md: '20px' }}
+        lineHeight={{ base: '29px', md: '24px' }}
+      >
+        Распознаваемые паттерны
+      </Text>
+      <CustomIcon imageSrc="./assets/images/svg/patterns-list.svg" alt="List" />
+    </Flex>
+    <CustomIcon
+      imageSrc="./assets/images/svg/recognizable-patterns.svg"
+      alt="Patterns"
+    />
+  </Flex>
+)
 
-export default RecognizablePatterns;
+export default RecognizablePatterns
