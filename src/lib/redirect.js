91e3171192dbea2530ec5d6a12f07fcf1f90a32f
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2023-06-23 16:38:31 +0600

    test

--- src/lib/redirect.js
+++ src/lib/redirect.js
@@ -1,23 +1,24 @@
-import languageDetector from '@/lib/languageDetector'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import languageDetector from './languageDetector'
 
 export const useRedirect = (to) => {
   const router = useRouter()
   to = to || router.asPath
+
   // language detection
   useEffect(() => {
     const detectedLng = languageDetector.detect()
-    if (to.startsWith('/' + detectedLng)) {
-      console.log({ to, detectedLng, router: router.route })
-      // && router.route === '/404'
+    if (to.startsWith('/' + detectedLng) && router.route === '/404') {
+      console.log({ to, detectedLng, route: router.route })
+      // prevent endless loop
       router.replace('/' + detectedLng + router.route)
       return
     }
-
+    console.log({ to, detectedLng, route: router.route })
     languageDetector.cache(detectedLng)
     router.replace('/' + detectedLng + to)
-  }, [to, router.route, router])
+  })
 
   return <></>
 }
