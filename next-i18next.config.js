29cfe68366dc57fbdad3b6ca2dd572c67b45ecf4
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2023-06-23 18:56:16 +0600

    test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,6 @@ module.exports = {
   i18n: {
     locales: ['en', 'ru'],
     defaultLocale: 'en',
+    localeDetection: false,
   },
 }
--- src/lib/redirect.js
+++ src/lib/redirect.js
@@ -6,29 +6,20 @@ export const useRedirect = (to) => {
   const router = useRouter()
   to = to || router.asPath
 
-  // language detection
-  useEffect(() => {
+  const redirectTo = (to) => {
     const detectedLng = languageDetector.detect()
     if (to.startsWith('/' + detectedLng) && router.route === '/404') {
-      console.log('first', {
-        to,
-        detectedLng,
-        route: router.route,
-        asPath: router.asPath,
-      })
-      // prevent endless loop
       router.replace('/' + detectedLng + router.route)
       return
     }
-    console.log('second: ', {
-      to,
-      detectedLng,
-      route: router.route,
-      asPath: router.asPath,
-    })
+
     languageDetector.cache(detectedLng)
     router.replace('/' + detectedLng + to)
-  })
+  }
+
+  useEffect(() => {
+    redirectTo(to)
+  }, [])
 
   return <></>
 }
